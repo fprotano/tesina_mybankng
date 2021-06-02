9b9fcf5a4cd888755a398c9b05b7a8b880811adb
luca- <luca-@DESKTOP-OANKCVL>
2021-06-02 12:08:20 +0200

    ciao

--- src/app/components/helpCenter/help-center/help-center.component.ts
+++ src/app/components/helpCenter/help-center/help-center.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { HelpCenterThread } from 'src/app/models/help-center-thread';
+import { Superclasse } from 'src/superclasse';
+import { AccountService } from 'src/app/services/account/account.service';
+import { StaffService } from 'src/app/services/staff/staff.service';
+import { Router } from '@angular/router';
+import { HelpCenterService } from 'src/app/services/helpCenter/help-center.service';
 
 @Component({
   selector: 'app-help-center',
   templateUrl: './help-center.component.html',
   styleUrls: ['./help-center.component.css']
 })
-export class HelpCenterComponent implements OnInit {
+export class HelpCenterComponent extends Superclasse implements OnInit {
 
-  constructor() { }
+
+  // tslint:disable-next-line: new-parens
+  threads: Array<HelpCenterThread> = new Array();
+  constructor(private staffservice:StaffService,router: Router, accountService: AccountService, protected helpcenterservice : HelpCenterService) {
+	super(router, staffservice, accountService);
+ }
 
   ngOnInit() {
+    this.helpcenterservice.findThreads(this.findSuccess.bind(this), this.findFailure.bind(this));
   }
 
+
+  findSuccess(data: any) {
+		this.threads = data;
+	}
+
+
+	findFailure(err: String, err_code: String) {
+		alert("Errore , email gia' esistente");
+	}
 }
--- src/app/services/helpCenter/help-center.service.ts
+++ src/app/services/helpCenter/help-center.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HelpCenterService {
+export class HelpCenterService extends ApiService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+		super(http);
+	}
+
+  findThreads(callbackOnSuccess,callbackOnFailure){
+    this.doGet("helpCenter/findAllThreads",callbackOnSuccess,callbackOnFailure);
+  }
 }
