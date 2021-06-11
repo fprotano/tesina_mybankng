09b69f64b6248e0d060084cb197198257e2121b5
Exolab <Exolab@DESKTOP-LSKSALQ>
2021-06-11 11:10:47 +0200

    list faq

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FakeComponent } from './components/fake/fake.component';
 import { FakeInsertComponent } from './components/fake-insert/fake-insert.component';
-import { FakeLoginComponent } from './components/fake-login/fake-login.component'; 
+import { FakeLoginComponent } from './components/fake-login/fake-login.component';
 
 import { FakeLogoutComponent } from './components/fake-logout/fake-logout.component';
 import { RegistrazioneComponent } from './components/Staff/registrazione/registrazione.component';
