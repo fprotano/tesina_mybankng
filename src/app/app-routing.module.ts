9807a69f77f28c14013db4785d0887950f6e0db6
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-15 11:54:29 +0200

    parti vecchie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core'; 
 import { Routes, RouterModule } from '@angular/router';
-import { FakeComponent } from './components/fake/fake.component';
-import { FakeInsertComponent } from './components/fake-insert/fake-insert.component';
-import { FakeLoginComponent } from './components/fake-login/fake-login.component';
-
-import { FakeLogoutComponent } from './components/fake-logout/fake-logout.component';
-import { RegistrazioneComponent } from './components/Staff/registrazione/registrazione.component';
-import { HomestaffComponent } from './components/Staff/homestaff/homestaff.component';
 import { RegistrazioneAccountComponent } from './components/account/registrazione-account/registrazione-account.component';
 import { LoginAccountComponent } from './components/account/login-account/login-account.component';
-import { LoginComponent } from './components/Staff/login/login.component';
 import { HomeComponent } from './components/account/home/home.component';
 import { HelpCenterComponent } from './components/helpCenter/help-center/help-center.component';
-import { ListExternalTransactionsComponent } from './components/Staff/externalTransactions/list-external-transactions/list-external-transactions.component';
 import { ListFaqComponent } from './components/faq/list-faq/list-faq.component';
 import { AddFaqComponent } from './components/faq/add-faq/add-faq.component';
 import { VisualizzaInformazioniComponent } from './components/account/visualizza-informazioni/visualizza-informazioni.component';
@@ -22,18 +13,10 @@ import { PaymentComponent } from './components/payment/payment.component';
 
 const routes: Routes = [
 	 {path: '', redirectTo: '/login-account', pathMatch: 'full'},
-	{ path: "register", component: RegistrazioneComponent }
-	,{path:  "login", component: LoginComponent  }
-	, { path: "homestaff",  component: HomestaffComponent  }
-	, { path: "fake",  component:  FakeComponent }
-	, { path: "fake-insert", component: FakeInsertComponent }
-	, { path: "fake-login", component: FakeLoginComponent }
-	, { path: "fake-logout", component: FakeLogoutComponent }
 	, { path: "registrazione-account", component: RegistrazioneAccountComponent }
 	, { path: "login-account", component: LoginAccountComponent }
 	, { path: "home", component: HomeComponent }
 	, { path: "help-center", component: HelpCenterComponent }
-	, { path: "Staff/transactions/list", component: ListExternalTransactionsComponent }
 	, { path: "list-faq", component: ListFaqComponent }
 	, {path: "Inf-personali-account", component:  VisualizzaInformazioniComponent}
 	, {path: "cambia-password",component:CambiaPasswordComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { FakeComponent } from './components/fake/fake.component';
-import { FakeInsertComponent } from './components/fake-insert/fake-insert.component';
+
 import { NavComponent } from './components/nav/nav.component';
-import { FakeLoginComponent } from './components/fake-login/fake-login.component';
-import { FakeLogoutComponent } from './components/fake-logout/fake-logout.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { RegistrazioneComponent } from './components/Staff/registrazione/registrazione.component';
-import { HomestaffComponent } from './components/Staff/homestaff/homestaff.component';
+
 import { RegistrazioneAccountComponent } from './components/account/registrazione-account/registrazione-account.component';
 import { LoginAccountComponent } from './components/account/login-account/login-account.component';
 import { HomeComponent } from './components/account/home/home.component';
-import { LoginComponent } from './components/Staff/login/login.component';
+
 import { HelpCenterComponent } from './components/helpCenter/help-center/help-center.component';
-import { ListExternalTransactionsComponent } from './components/Staff/externalTransactions/list-external-transactions/list-external-transactions.component';
 import { ListFaqComponent } from './components/faq/list-faq/list-faq.component';
 import { AddFaqComponent } from './components/faq/add-faq/add-faq.component';
 import { VisualizzaInformazioniComponent } from './components/account/visualizza-informazioni/visualizza-informazioni.component';
@@ -28,19 +24,14 @@ import { PaymentComponent } from './components/payment/payment.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FakeComponent,
-    FakeInsertComponent,
+    
     NavComponent,
-    FakeLoginComponent,
-    FakeLogoutComponent,
-    RegistrazioneComponent,
-    HomestaffComponent,
-    LoginComponent,
+    
     RegistrazioneAccountComponent,
     LoginAccountComponent,
     HomeComponent,
     HelpCenterComponent,
-    ListExternalTransactionsComponent,
+    
     ListFaqComponent,
     VisualizzaInformazioniComponent,
     CambiaPasswordComponent,
