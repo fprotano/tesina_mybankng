9dd7e37f24b15430352045438e689535c1faafae
luca- <luca-@LAPTOP-TFS01PTJ.fritz.box>
2021-06-16 11:58:36 +0200

    sdsad

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -39,7 +39,7 @@ export class PaymentComponent extends Superclasse implements OnInit {
     this.myform.nativeElement.urlSuccess.value = this.payment.urlSuccess;
     this.myform.nativeElement.urlUnDo.value = this.payment.urlUndo;
     this.myform.nativeElement.urlNotify.value = this.payment.urlNotify;
-
+    console.log(this.payment);
    this.myform.nativeElement.submit();
   }
 
